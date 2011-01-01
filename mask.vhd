6b45e7526db157d48b4ad19d805a7ed3396a065c
Matthew Wampler-Doty <negacthulhu@gmail.com>
2011-01-01 13:44:58 -0700

    data_process is now event driven; only has two states

--- mask.vhd
+++ mask.vhd
@@ -1,12 +1,4 @@
 -- mask.vhd
--- 800 bit version
--- start with events and mask in memory; 
--- process events with mask and generate image
---
--- inputs: cmd_start is triggered by 'cmd_start' signal going high
---
--- outputs: 'finished' goes high to indicate when process is complete
--- 
 
 library ieee;
 use ieee.std_logic_1164.all;
@@ -22,57 +14,54 @@ use work.common_decs.all;
 use work.rom.all;
 
 entity data_process is
-  port (clk       : in  std_logic;
-        reset     : in  std_logic;
-        --waiting   : in  std_logic;
-        --event     : in  event_type;
-        finished  : out std_logic;
-        image_out : out image_array);
+  port (
+    -- The clock
+    clk       : in  std_logic;
+    -- Flag for resetting the device
+    reset     : in  std_logic;
+    -- An event register
+    event     : in  event_type;
+    -- Flag which indicates that a new event is waiting
+    waiting   : in  std_logic;
+    -- Flag which pulses when we become idle
+    idle : out std_logic;
+    -- The image array
+    image_out : out image_array);
 end data_process;
 
 architecture syn of data_process is
-  signal state_reg : integer range 0 to 5              := 0;
-  signal shiftreg  : bit_vector (0 to (rom'length -1)) := (others => '0');
-
+  signal state_reg : integer range 1 to 2 := 1;
+  signal shiftreg  : bit_vector (0 to (rom'length -1));
+  signal image   : image_array;
+  
   -- These are all calculated in terms of the events array
   constant MINVAL   : integer := 692;
-  constant MAXVAL   : integer := 942;
-  constant MAXLOOPS : integer := events_array'length;
 
   signal nshifts : integer range 0 to 4095;
-  signal done    : std_logic   := '0';
-  signal nloops  : integer range 0 to 750;
-  signal nval    : integer range 0 to 500;
-  signal image   : image_array := (others => 0);
   
 begin
   process (clk)
   begin
     if rising_edge(clk) then
       if (reset = '1') then
-        done  <= '0';
+        state_reg <= 1;
         image <= (others => 0);
+        shiftreg <= (others => '0');
+        idle <= '1';
       else
         case state_reg is
-          when 0 =>
-            if (done = '0') then
-              state_reg <= 1;
-              nloops    <= 0;
-            end if;
---  load shift register and determine number of shifts
           when 1 =>
-            if (done = '0') then
-              shiftreg  <= ROM;
-              nshifts   <= events(nloops) - MINVAL;
-              nloops    <= nloops + 1;
-              state_reg <= 2;
-            end if;
---   shift the SR 
+  -- If there's data waiting, prepare the barrel shifter
+  -- and leave the idle state
+              if waiting = '1' then
+               idle <= '0';
+               shiftreg  <= ROM sll (event - MINVAL);
+               state_reg <= 2;
+              end if; 
+
+--  loop through each bit of the barrel shifter
+--  and change image accordingly
           when 2 =>
-            shiftreg  <= shiftreg sll nshifts;
-            state_reg <= 3;
---  loop through each bit of SR and change image according to SR bit       
-          when 3 =>
             for i in 0 to (image_size) loop
               if shiftreg(i) = '1' then
                 image(i) <= image(i) + 4;
@@ -80,23 +69,11 @@ begin
                 image(i) <= image(i) - 1;
               end if;
             end loop;
-            state_reg <= 4;
---  determine whether all loops have been done
-          when 4 =>
-            if (nloops > (MAXLOOPS-1)) then
-              state_reg <= 5;
-            else
-              state_reg <= 1;
-            end if;
-
---  output 250 image elements then its done   
-          when 5 =>
-            done      <= '1';
-            state_reg <= 5;
+            idle <= '1';
+            state_reg <= 1;
         end case;
       end if;
     end if;
   end process;
   image_out <= image;
-  finished  <= done;
 end syn;
--- mask_tb.vhd
+++ mask_tb.vhd
@@ -10,42 +10,61 @@ architecture behav of mask_tb is
   component data_process is
     port (clk       : in  std_logic;
           reset     : in  std_logic;
-          --waiting   : in  std_logic;
-          --event     : in  event_type;
-          finished  : out std_logic;
+          event     : in  event_type;
+          waiting   : in  std_logic;
+          idle : out std_logic;
           image_out : out image_array);
   end component;
   -- The device under test is the data_process
   for dut          : data_process use entity work.data_process;
   signal clk       : std_logic;
   signal reset     : std_logic;
-  --signal waiting   : std_logic;
-  --signal event     : event_type;
-  signal finished  : std_logic;
+  signal waiting   : std_logic;
+  signal event     : event_type;
+  signal idle : std_logic;
   signal image_out : image_array;
 begin
   --  Component instantiation.
   dut : data_process
     port map (clk       => clk,
               reset     => reset,
-              --waiting   => waiting,
-              --event     => event,
-              finished  => finished,
+              event     => event,
+              waiting   => waiting,
+              idle => idle,
               image_out => image_out) ;
   process
-    variable l : line;
+    variable l      : line;
+    variable n : integer := 0;
+    variable oidle : std_logic;
   begin
     -- Reset the unit
     clk   <= '0';
     reset <= '1';
-    wait for 2 ns;
+    wait for 1 ns;
     clk   <= '1';
+    wait for 1 ns;
     reset <= '0';
-    while not (finished = '1') loop
-      wait for 2 ns;
+
+    -- Main loop
+    waiting <= '1';
+--    while finished /= '1' loop
+    while n < events'length loop
+      -- Fixme: Not very elegant
+      event <= events(n);
+      oidle := idle;
+      wait for 1 ns;
       clk <= not clk;
-      wait for 2 ns;
+      wait for 1 ns;
+      
+      -- A poor man's rising_edge()
+      if (idle='1' and idle /= oidle) then
+--        report integer'image(n);
+        n := n + 1;
+      end if;
     end loop;
+    waiting <= '0';
+
+    -- Output results
     for i in 0 to image_size loop
       write (l, string'(integer'image(image_out(i))));
       writeline (output, l);
