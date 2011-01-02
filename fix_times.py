26c08f0f1258db7b9c56764209024b6b270a4c99
Matthew Wampler-Doty <negacthulhu@gmail.com>
2011-01-01 19:57:39 -0700

    Made python script to fix events.dat files so that the minimum value is zero

---
+++ fix_times.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import sys
+import csv
+import itertools
+
+if (len(sys.argv) > 1) : fin = open(sys.argv[1],"r")
+else : fin = sys.stdin
+
+if (len(sys.argv) > 2) : fout = open(sys.argv[2],"w")
+else : fout = sys.stdout
+
+events = csv.reader(fin, delimiter=' ')
+events_fixed = map(int, filter(lambda x : x != "", 
+                       list(itertools.chain(*events))))
+min_event = min(events_fixed)
+for event in events_fixed:
+    print >>fout, event - min_event
