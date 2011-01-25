e8eb553faca2972fee3b69a2ec312d0192af0442
Matthew Wampler-Doty <negacthulhu@gmail.com>
2011-01-25 14:06:35 +0900

    Beautified using emacs

--- wishbone_CCD_simulator/vhdl/fifo.vhdl
+++ wishbone_CCD_simulator/vhdl/fifo.vhdl
@@ -13,19 +13,19 @@ use IEEE.STD_LOGIC_UNSIGNED.all;
 
 entity fifo_syn is
   generic (
-    width        :     integer := 16;
-    addrdepth    :     integer := 10);   -- Depth of the FIFO: 2^addrdepth
+    width     : integer := 16;
+    addrdepth : integer := 10);         -- Depth of the FIFO: 2^addrdepth
   port (
-    ClkxC        : in  std_logic;
-    din          : in  std_logic_vector(width-1 downto 0);
-    rd_en        : in  std_logic;
-    ResetxRB     : in  std_logic;
-    wr_en        : in  std_logic;
-    data_count   : out std_logic_vector(addrdepth-1 downto 0);
-    dout         : out std_logic_vector(width-1 downto 0);
-    empty        : out std_logic;
-    full         : out std_logic;
-    wr_ack       : out std_logic);
+    clk        : in  std_logic;
+    din        : in  std_logic_vector(width-1 downto 0);
+    rd_en      : in  std_logic;
+    reset      : in  std_logic;
+    wr_en      : in  std_logic;
+    data_count : out std_logic_vector(addrdepth-1 downto 0);
+    dout       : out std_logic_vector(width-1 downto 0);
+    empty      : out std_logic;
+    full       : out std_logic;
+    wr_ack     : out std_logic);
 end fifo_syn;
 
 architecture fifo_syn_a of fifo_syn is
@@ -35,7 +35,7 @@ architecture fifo_syn_a of fifo_syn is
 
   signal RdCntr : std_logic_vector(addrdepth-1 downto 0);
   signal WrCntr : std_logic_vector(addrdepth-1 downto 0);
-  signal DCntr : std_logic_vector(addrdepth-1 downto 0);
+  signal DCntr  : std_logic_vector(addrdepth-1 downto 0);
 
   signal RW      : std_logic_vector(1 downto 0);
   signal fullxB  : std_logic;
@@ -43,19 +43,19 @@ architecture fifo_syn_a of fifo_syn is
 begin
   RW <= rd_en & wr_en;
 
-  seq : process (ClkxC, ResetxRB)
+  seq : process (clk, reset)
   begin  -- process seq
-    if ResetxRB = '0' then              -- asynchronous reset (active low)
-      RdCntr                        <= (others => '0');
-      WrCntr                        <= (others => '0');
-      Dcntr                         <= (others => '0');
-      emptyxB                       <= '0';
-      fullxB                        <= '1';
-      wr_ack                        <= '0';
-    elsif ClkxC'event and ClkxC = '1' then  -- rising clock edge
-      wr_ack                        <= '0';
+    if reset = '0' then                 -- asynchronous reset (active low)
+      RdCntr  <= (others => '0');
+      WrCntr  <= (others => '0');
+      Dcntr   <= (others => '0');
+      emptyxB <= '0';
+      fullxB  <= '1';
+      wr_ack  <= '0';
+    elsif rising_edge(clk) then         -- rising clock edge
+      wr_ack <= '0';
       case RW is
-        when "00"                              =>  -- read and write at the same time
+        when "00" =>                    -- read and write at the same time
           RdCntr                    <= RdCntr + 1;
           WrCntr                    <= WrCntr + 1;
           wr_ack                    <= '1';
@@ -66,34 +66,34 @@ begin
             if ((RdCntr +1) = WrCntr) then
               emptyxB <= '0';
             end if;
-            RdCntr    <= RdCntr + 1;
-	    DCntr     <= DCntr - 1;
+            RdCntr <= RdCntr + 1;
+            DCntr  <= DCntr - 1;
           end if;
-          fullxB      <= '1';
-	  wr_ack      <= '0';
+          fullxB <= '1';
+          wr_ack <= '0';
 
         when "10" =>                    -- only write
-          emptyxB                     <= '1';
+          emptyxB <= '1';
           if fullxB = '1' then          -- not full
             reg(conv_integer(WrCntr)) <= din;
             if WrCntr+1 = RdCntr then
-              fullxB                  <= '0';
+              fullxB <= '0';
             end if;
-            WrCntr                    <= WrCntr +1;
-	    DCntr                     <= DCntr + 1;
-            wr_ack                    <= '1';
+            WrCntr <= WrCntr +1;
+            DCntr  <= DCntr + 1;
+            wr_ack <= '1';
           end if;
 
-        when others => 
+        when others =>
           wr_ack <= '0';
       end case;
     end if;
   end process seq;
 
-  dout         <= reg(conv_integer(RdCntr));
-  full         <= not fullxB;
-  empty        <= not emptyxB;
-  data_count   <= DCntr;
+  dout       <= reg(conv_integer(RdCntr));
+  full       <= not fullxB;
+  empty      <= not emptyxB;
+  data_count <= DCntr;
 
 end fifo_syn_a;
 
