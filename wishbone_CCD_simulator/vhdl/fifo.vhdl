bd27de4ab336ef9f98318acea3ac97418c1882ea
Matthew Wampler-Doty <negacthulhu@gmail.com>
2011-01-31 11:29:16 +0900

    Inverted logic & changed the way queue size is calculated

--- wishbone_CCD_simulator/vhdl/fifo.vhdl
+++ wishbone_CCD_simulator/vhdl/fifo.vhdl
@@ -14,16 +14,21 @@ use IEEE.STD_LOGIC_UNSIGNED.all;
 entity fifo_syn is
   generic (
     width     : integer := 16;
-    addrdepth : integer := 10;
-    memdepth  : integer := 1023);         -- Depth of the FIFO: 2^addrdepth-1
+    addrdepth : integer := 10);         -- Depth of the FIFO: 2 ** addrdepth-1
   port (
-    clk        : in  std_logic;
-    din        : in  std_logic_vector(width-1 downto 0);
-    rd_en      : in  std_logic;
-    reset      : in  std_logic;
-    wr_en      : in  std_logic;
-    data_count : out std_logic_vector(addrdepth-1 downto 0);
+    -- Inputs
+    ---- Global signals
+    clk   : in std_logic;
+    reset : in std_logic;
+    ---- Data
+    din   : in std_logic_vector(width-1 downto 0);
+    ---- Flags for Read and Write instructions
+    rd_en : in std_logic;
+    wr_en : in std_logic;
+
+    -- Outputs
     dout       : out std_logic_vector(width-1 downto 0);
+    data_count : out std_logic_vector(addrdepth-1 downto 0);
     empty      : out std_logic;
     full       : out std_logic;
     wr_ack     : out std_logic);
@@ -31,8 +36,9 @@ end fifo_syn;
 
 architecture fifo_syn_a of fifo_syn is
   subtype wrdtype is std_logic_vector(width-1 downto 0);
+  constant memdepth : integer := 2 ** addrdepth - 1;
   type regtype is array (0 to memdepth) of wrdtype;
-  signal reg : regtype;
+  signal reg        : regtype;
 
   signal RdCntr : std_logic_vector(addrdepth-1 downto 0);
   signal WrCntr : std_logic_vector(addrdepth-1 downto 0);
@@ -50,50 +56,51 @@ begin
       RdCntr  <= (others => '0');
       WrCntr  <= (others => '0');
       Dcntr   <= (others => '0');
-      emptyxB <= '0';
-      fullxB  <= '1';
+      emptyxB <= '1';
+      fullxB  <= '0';
       wr_ack  <= '0';
     elsif rising_edge(clk) then         -- rising clock edge
-      wr_ack <= '0';
       case RW is
-        when "00" =>                    -- read and write at the same time
+        when "11" =>                    -- read and write at the same time
           RdCntr                    <= RdCntr + 1;
           WrCntr                    <= WrCntr + 1;
           wr_ack                    <= '1';
           reg(conv_integer(WrCntr)) <= din;
 
-        when "01" =>                    -- only read
-          if (emptyxB = '1') then       -- not empty
+        when "10" =>                    -- only read
+          if (emptyxB = '0') then       -- not empty
             if ((RdCntr +1) = WrCntr) then
-              emptyxB <= '0';
+              emptyxB <= '1';
             end if;
             RdCntr <= RdCntr + 1;
             DCntr  <= DCntr - 1;
           end if;
-          fullxB <= '1';
+          fullxB <= '0';
           wr_ack <= '0';
 
-        when "10" =>                    -- only write
+        when "01" =>                    -- only write
           emptyxB <= '1';
-          if fullxB = '1' then          -- not full
+          if fullxB = '0' then          -- not full
             reg(conv_integer(WrCntr)) <= din;
             if WrCntr+1 = RdCntr then
-              fullxB <= '0';
+              fullxB <= '1';
             end if;
             WrCntr <= WrCntr +1;
             DCntr  <= DCntr + 1;
             wr_ack <= '1';
           end if;
-
-        when others =>
+          
+        when "00" =>
           wr_ack <= '0';
+        when others => null;
       end case;
     end if;
   end process seq;
 
-  dout       <= reg(conv_integer(RdCntr));
-  full       <= not fullxB;
-  empty      <= not emptyxB;
+  dout <= reg(conv_integer(RdCntr)) when emptyxB = '0'
+          else (others => '0');
+  full       <= fullxB;
+  empty      <= emptyxB;
   data_count <= DCntr;
 
 end fifo_syn_a;
