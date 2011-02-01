930967b728610bb8c98d99bbea82de4c2f7d6740
Matthew Wampler-Doty <negacthulhu@gmail.com>
2011-02-01 20:54:07 +0900

    data_bridge feeds into fifo now

--- wishbone_CCD_simulator/vhdl/data_bridge.vhdl
+++ wishbone_CCD_simulator/vhdl/data_bridge.vhdl
@@ -7,20 +7,19 @@ use work.common_decs.all;
 entity data_bridge is
   port (
     -- Global clock
-    clk                    : in  std_logic;
+    clk    : in  std_logic;
     -- Strobe clock from physical pin on board
-   STROBE                 : in  std_logic ;
+    STROBE : in  std_logic;
+    -- Write instruction
+    wr_en  : out std_logic;
     -- Data Input
-   a, b                   : in  std_logic_vector(7 downto 0) ;
-    -- IRQ Flag
-   irqport                    : out std_logic ;
-    -- Wishbone Signals
-   wbw : in wbw ;
-   wbr : out wbr 
+    a, b   : in  std_logic_vector (7 downto 0);
+    -- Data Output
+    dout   : out std_logic_vector (chan_size-1 downto 0)
     );
-end; 
+end;
 
-architecture data_bridge_1 of data_bridge is
+architecture RTU of data_bridge is
   -- Previous state of the STROBE pin according to the clk
   signal previous_STROBE : std_logic := STROBE;
 begin
@@ -28,26 +27,12 @@ begin
   begin
     if (rising_edge(clk)) then
       if (STROBE /= previous_STROBE) then  -- If change in STROBE value
-        irqport             <= '1';            -- throw interupt
+        wr_en           <= '1';            -- Pulse a read instruction
         previous_STROBE <= STROBE;
       else
-        irqport <= '0';                        -- ...otherwise make interupt quiet
+        wr_en <= '0';
       end if;
     end if;
   end process strobe_emit;
-
-  wb_interact : process(clk)
-  begin
-    if (rising_edge(clk)) then
-      if(wbw.strobe = '1' and wbw.writing = '0' and wbw.cycle = '1') then
-        -- If wishbone is ready, dump data
-        wbr.ack      <= '1';
-        wbr.readdata <= a & b;
-      else
-        -- Otherwise remain quiet
-        wbr.ack      <= '0';
-        wbr.readdata <= (others => '0');
-      end if;
-    end if;
-  end process;
+  dout <= a & b;
 end;
