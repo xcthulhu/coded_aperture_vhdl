01daef1c943d9425ed9aa3038cff2a3e0d14c7d1
Matthew Wampler-Doty <negacthulhu@gmail.com>
2011-02-01 20:51:11 +0900

    Fixed so intercon controls MUX

--- wishbone_CCD_simulator/vhdl/common_decs.vhdl
+++ wishbone_CCD_simulator/vhdl/common_decs.vhdl
@@ -3,32 +3,31 @@ use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
 package common_decs is
+  
+  constant chan_size : integer := 16;   -- Size of data channel
+  subtype device_id is std_logic_vector(chan_size-1 downto 0);
+  
   -- Wishbone Interface Signals
-  type wbr is                                  -- Wishbone read system
+  type wbr is                                    -- Wishbone read system
   record
-    readdata : std_logic_vector(15 downto 0);  -- Data bus read by wishbone
-    ack      : std_logic;                      -- Acknowledge
+    readdata : std_logic_vector (chan_size-1 downto 0);  -- Data bus read by wishbone
+    ack      : std_logic;  -- Acknowledge
   end record;
 
-  constant addr_id_top_bit : integer := 12 ;
-  constant addr_id_bot_bit : integer := 11 ;
-  subtype addr_id is std_logic_vector(addr_id_top_bit downto addr_id_bot_bit);
-
-  type wbw is                                   -- Wishbone write system
+  type wbw is                                    -- Wishbone write system
   record
-    strobe    : std_logic;                      -- Data Strobe
-    writing   : std_logic;                      -- Busy writing
-    cycle     : std_logic;                      -- Bus cycle in progress
-    address   : std_logic_vector(12 downto 0);  -- Address bus
-    writedata : std_logic_vector(15 downto 0);  -- Data bus written by wishbone
+    strobe    : std_logic;                       -- Data Strobe
+    writing   : std_logic;                       -- Busy writing
+    cycle     : std_logic;                       -- Bus cycle in progress
+    address   : std_logic_vector (12 downto 0);  -- Address bus
+    writedata : std_logic_vector (chan_size-1 downto 0);  -- Data bus written by wishbone
   end record;
 
   -- Methods for checking for access to the wishbone bus
   ---- For wbw.writing = '0'
-  function check_wb0(wbw : wbw ; addr_id : addr_id) return boolean ;
+  function check_wb0 (wbw : wbw) return boolean ;
   ---- For wbw.writing = '1'
-  function check_wb1(wbw : wbw ; addr_id : addr_id) return boolean ;
-
+  function check_wb1 (wbw : wbw) return boolean ;
    -- i.MX Control Signals
    type imx_in is
    record
@@ -37,19 +36,15 @@ package common_decs is
       oe_n      : std_logic;
       eb3_n     : std_logic;
    end record;
-
 end package;
 
 package body common_decs is
-
-  function check_wb0(wbw : wbw ; addr_id : addr_id) return boolean is
-  begin return ((wbw.address(addr_id_top_bit downto addr_id_bot_bit) = addr_id)
-                and wbw.strobe = '1' and wbw.cycle = '1' and wbw.writing = '0');
+  function check_wb0(wbw : wbw) return boolean is
+  begin return (wbw.strobe = '1' and wbw.cycle = '1' and wbw.writing = '0');
   end;
 
-  function check_wb1(wbw : wbw ; addr_id : addr_id) return boolean is
-  begin return ((wbw.address(addr_id_top_bit downto addr_id_bot_bit) = addr_id)
-                and wbw.strobe = '1' and wbw.cycle = '1' and wbw.writing = '1');
+  function check_wb1 (wbw : wbw) return boolean is
+  begin return (wbw.strobe = '1' and wbw.cycle = '1' and wbw.writing = '1');
   end;
 
 end package body common_decs;
