87ee3b2784c29671a6b767a499ccf7235d1be649
Matthew Wampler-Doty <negacthulhu@gmail.com>
2011-03-03 09:02:18 -0700

    Bitmap compiles now

--- wishbone_CCD_simulator/fpgaregs_debug/top_mod.vhdl
+++ wishbone_CCD_simulator/fpgaregs_debug/top_mod.vhdl
@@ -15,29 +15,34 @@ entity top_mod is
       irq         : out   std_logic;
       -- Armadeus handshaking
       imx_data    : inout imx_chan;
-      imx_address : in std_logic_vector(11 downto 0);  -- LSB not used 
-      imx_cs_n    : in std_logic;
-      imx_oe_n    : in std_logic;
-      imx_eb3_n   : in std_logic
+      imx_address : in    std_logic_vector(11 downto 0);  -- LSB not used 
+      imx_cs_n    : in    std_logic;
+      imx_oe_n    : in    std_logic;
+      imx_eb3_n   : in    std_logic
       );
 end entity;
 
 architecture RTL of top_mod is
-   signal read_data : imx_chan;
+  signal read_data : imx_chan;
+  signal readf     : std_logic;
+  signal strobe    : std_logic;
 begin
-  process(clk) begin
-	if rising_edge(clk) then
-		case imx_address is
-			when "000000000001" => read_data <= x"F00F";
-			when "000000000010" => read_data <= x"1337";
-			when "000000000011" => read_data <= x"D0D0";
-			when "000000000100" => read_data <= x"DEAD";
-			when others => read_data <= (others => '0');
-		end case;
-	end if;
+  readf  <= not (imx_cs_n or imx_oe_n);
+  strobe <= not (imx_cs_n) and not(imx_oe_n and imx_eb3_n);
+  process(clk)
+  begin
+    if rising_edge(clk) then
+      case imx_address is
+        when "000000000001" => read_data <= x"F00F";
+        when "000000000010" => read_data <= x"1337";
+        when "000000000011" => read_data <= x"D0D0";
+        when "000000000100" => read_data <= x"DEAD";
+        when others         => read_data <= (others => '0');
+      end case;
+    end if;
   end process;
 
-  imx_data <= read_data when (not (imx_cs_n or imx_oe_n) = '1') 
-			else (others => 'Z');
+  imx_data <= read_data when ((readf and strobe) = '1')
+              else (others => 'Z');
 
 end architecture;
