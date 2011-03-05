484b05ad066b10ddaf98c130d3bfdd9137dbf270
noqsi <jpd@noqsi.com>
2011-03-04 18:07:53 -0700

    Finished first draft of mmio user driver. Fixed error in address list.

--- wishbone_CCD_simulator/user/sxi_clocks_mmio.c
+++ wishbone_CCD_simulator/user/sxi_clocks_mmio.c
@@ -1,6 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <fcntl.h>
+
+// How many words to grab
+
 #define COUNT 2000000
 
-static void *regp;
+// Register address offsets
+
+#define C_ID	0x8
+#define	C_COUNT	0xA
+#define C_DATA	0xC
+#define C_BAD	0xE
+
+// Identify the firmware
+
+#define C_ID_NUM	0x0523
+
+// Mapping of FPGA in the Armadeus
+
+#define FPGA_ADDRESS 0xD6000000
+
+static volatile void *regp;
 
 // We need to avoid page faults during acquision.
 // Otherwise we could lose data.
@@ -9,22 +34,23 @@ static void *regp;
 // to write bits into the buffer to get it into primary
 // memory.
 
-uint16_t *getbuf( size_t size )
+static uint16_t *getbuf( size_t size )
 {
 	size_t bytes = size * sizeof( uint16_t );
 	uint16_t *b = malloc( bytes );
 	
 	if( !b ) {
-		perror( ME );
+		perror( "getbuf" );
 		exit( EXIT_FAILURE );
 	}
 	
 	return memset( b, 0xaa, bytes );
 }
 
+
 // Get a pointer to the registers the FPGA firmware implements
 
-void *locate_regs( void )
+static void *locate_regs( void )
 {
 	void *ra;
 	int fd = open("/dev/mem", O_RDWR|O_SYNC);
@@ -45,29 +71,82 @@ void *locate_regs( void )
 }
 
 
+// FPGA register access
+
 static inline uint16_t getreg( int reg )
 {
-	return * (uint16_t *) (regp + reg);
+	return * (volatile uint16_t *) (regp + reg);
 }
 
 
 static inline void putreg( int reg, uint16_t val )
 {
-	* (uint16_t *) (regp + reg) = val;
+	* (volatile uint16_t *) (regp + reg) = val;
 }
 
 
-int main(int argc, char **argv, **envp)
+// If the ID register is wrong, give up
+
+static void checkID( void )
 {
-	uint16_t *buf = getbuf( COUNT );
+	uint16_t id = getreg( C_ID );
+	
+	if( C_ID_NUM != C_ID ) {
+		fprintf( stderr, "Id 0x%x != expected 0x%x\n", id, C_ID_NUM );
+		exit( EXIT_FAILURE );
+	}
+}
+
+
+// Read a bufferful
+
+static void getem( uint16_t *buf, int count )
+{
+	while( count-- ) {
+		while( !getreg( C_COUNT )) ;	/* spin */
+		*buf++ = getreg( C_DATA );
+	}
+}
+
+
+// Write a bufferful
+
+static void dumpem( uint16_t *buf, int count )
+{
+	int r = write( 1, buf, count * sizeof( uint16_t ));
+
+	if( r < 0 ) {
+		perror( "Can't output" );
+		exit( EXIT_FAILURE );
+	}
+}
+
+
+// If you're not root, this won't work, but just print a warning	
+
+static void priority( void )
+{
+	if( -20 != nice( -20 )) perror( "priority" );
+}
+
+
+int main(int argc, char **argv, char **envp)
+{
+	uint16_t *buf;
+	
+	priority();	// raise priority to avoid data loss
+	
+	buf = getbuf( COUNT );
 	
 	regp = locate_regs();
 	
-	getem( regs, buf, COUNT);
+	checkID();
 	
-	dumpem( buf, count );
+	getem( buf, COUNT );
 	
-	exit( 0 );
+	dumpem( buf, COUNT );
+	
+	exit( EXIT_SUCCESS );
 }
 	
 	
