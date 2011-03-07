72177d00c9ed7c9c21324a48238739b2e8ca65ae
Matthew Wampler-Doty <negacthulhu@gmail.com>
2011-03-06 23:33:48 -0700

    Adding some debug code for CS4/DTACK

--- wishbone_CCD_simulator/fpgaregs_debug/top_mod.vhdl
+++ wishbone_CCD_simulator/fpgaregs_debug/top_mod.vhdl
@@ -15,10 +15,11 @@ entity top_mod is
       irq         : out   std_logic;
       -- Armadeus handshaking
       imx_data    : inout imx_chan;
-      imx_address : in    std_logic_vector(11 downto 0);  -- LSB not used 
+      imx_address : in    addr;         -- LSB not used 
       imx_cs_n    : in    std_logic;
       imx_oe_n    : in    std_logic;
-      imx_eb3_n   : in    std_logic
+      imx_eb3_n   : in    std_logic;
+      imx_dtack   : out   std_logic
       );
 end entity;
 
@@ -33,11 +34,21 @@ begin
   begin
     if rising_edge(clk) then
       case imx_address is
-        when "000000000001" => read_data <= x"F00F";
-        when "000000000010" => read_data <= x"1337";
-        when "000000000011" => read_data <= x"D0D0";
-        when "000000000100" => read_data <= x"DEAD";
-        when others         => read_data <= (others => '0');
+        when "000000000001" =>
+          imx_dtack <= '1';
+          read_data <= x"F00F";
+        when "000000000010" =>
+          imx_dtack <= '0';
+          read_data <= x"1337";
+        when "000000000011" =>
+          imx_dtack <= '1';
+          read_data <= x"D0D0";
+        when "000000000100" =>
+          imx_dtack <= '0';
+          read_data <= x"DEAD";
+        when others =>
+          imx_dtack <= '1';
+          read_data <= (others => '0');
       end case;
     end if;
   end process;
