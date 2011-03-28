9ab9bcdd144e28c4a33d375142112567c85cc1f0
Matthew Wampler-Doty <negacthulhu@gmail.com>
2011-03-28 15:16:33 -0600

    Changed clocksim code around

--- wishbone_CCD_simulator/clocksim-vhdl/top_mod.vhdl
+++ wishbone_CCD_simulator/clocksim-vhdl/top_mod.vhdl
@@ -11,62 +11,59 @@ use work.common_decs.all;
 entity top_mod is
   generic (div : natural := 100);
   port (CLK            : in  std_logic;
-        SCLK_anode     : out std_logic;
-        SCLK_cathode   : out std_logic;
-        STROBE_anode   : out std_logic;
-        STROBE_cathode : out std_logic;
-        A_anode        : out std_logic;
-        A_cathode      : out std_logic;
-        B_anode        : out std_logic;
-        B_cathode      : out std_logic);
+        SCLK_out, SCLK_outb     : out std_logic;
+        STROBE_out, STROBE_outb   : out std_logic;
+        A_out, A_outb        : out std_logic;
+        B_out, B_outb        : out std_logic);
 end;
 
 architecture RTL of top_mod is
-  signal iSCLK, iStrobe, iA, iB : std_logic;
-  signal isysc                  : syscon;
+  signal xSCLK_out, xSTROBE_out,
+    xA_out, xB_out : std_logic;
+  signal sysc                  : syscon;
 
   component rstgen_syscon
-    port (clk  : in  std_logic;
-          sysc : out syscon);
+    port ( clk  : in  std_logic;
+           sysc : out syscon);
   end component;
 
   component clocksim
     generic (div : natural := 100);
-    port (sysc               : in  syscon;
-          SCLK, STROBE, A, B : out std_logic);
+    port ( sysc               : in  syscon;
+           SCLK, STROBE, A, B : out std_logic);
   end component;
 
 begin
   SCLK_LVDS_OUT : OBUFDS
-    port map (I  => iSCLK,
-              O  => SCLK_anode,
-              OB => SCLK_cathode);
+    port map (I  => xSCLK_out,
+              O  => SCLK_out,
+              OB => SCLK_outb);
 
   STROBE_LVDS_OUT : OBUFDS
-    port map (I  => iSTROBE,
-              O  => STROBE_anode,
-              OB => STROBE_cathode);
+    port map (I  => xSTROBE_out,
+              O  => STROBE_out,
+              OB => STROBE_outb);
 
   A_LVDS_OUT : OBUFDS
-    port map (I  => iA,
-              O  => A_anode,
-              OB => A_cathode);
+    port map (I  => xA_out,
+              O  => A_out,
+              OB => A_outb);
 
   B_LVDS_OUT : OBUFDS
-    port map (I  => iB,
-              O  => B_anode,
-              OB => B_cathode);
+    port map (I  => xB_out,
+              O  => B_out,
+              OB => B_outb);
 
   RESETER : rstgen_syscon
     port map (CLK  => CLK,
-              sysc => isysc);
+              sysc => sysc);
 
   CLOCKSIMULATOR : clocksim
     generic map (div => div)
-    port map (sysc   => isysc,
-              SCLK   => iSCLK,
-              STROBE => iSTROBE,
-              A      => iA,
-              B      => iB);
+    port map (sysc   => xsysc_out,
+              SCLK   => xSCLK_out,
+              STROBE => xSTROBE_out,
+              A      => xA_out,
+              B      => xB_out);
 
 end;
